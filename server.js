622686301a8a4bdc59faf1e1d6879cc8daa372d9
Brandon <brandilla1337@gmail.com>
2016-01-26 01:42:48 -0500

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express'),
+    routes = require('./app/routes/index.js');
+
+var app = express();
+
+routes(app);
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000...');
+});
